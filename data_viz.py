24b69787e3fb83337c9838f8303461bca61f0818
Davit Martirosyan <dvmartirosyan@gmail.com>
2020-12-08 10:51:09 +0100

    add circle_plot version 2

--- data_viz.py
+++ data_viz.py
@@ -6,6 +6,8 @@ from utils import get_human_readable_quarter_name
 from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 
+import plotly.graph_objects as go
+
 
 def circle_time_series_plot(top_words,
 							n,
@@ -83,8 +85,101 @@ def circle_time_series_plot(top_words,
 
 	plt.title('Top-{} most frequent words/phrases searched per quarter'.format(n),fontsize=title_fontsize)    
 	plt.xticks(list(range(1,len(xaxis_ticks)+1)),xaxis_ticks,rotation='45',fontsize=xlabel_fontsize);
-	plt.yticks(list(range(1,n+1)),top_n_labels,fontsize=ylabel_fontsize);
+	plt.yticks(list(range(1,n+1)),top_n_labels[-n:],fontsize=ylabel_fontsize);
 
+
+
+
+def circle_time_series_plot_v2(top_words,n,colors,r=500):
+	"""
+	Produces a plot showing the top-n most frequent words/phrases searched per quarter. 
+	Each word is represnted with a circle, where bigger circle means more searches. 
+
+	Parameters
+	----------
+	top_words : dict
+		Dictionary containing top-n words with corresponding frequences per each quarter. 
+
+	n : int
+		Number of top-n words to consider. 
+
+	colors : list
+		List of colours for the circles. Should contain number of colours equal to n.  
+
+	r : int or float, optional
+		Coefficient to control the size of circles. Higher coefficient creates smaller circles. r must be greater than or equal to 1.
+	"""	
+	labels = []
+	vals = []
+
+	for k, v in top_words.items():
+		for e in v:
+			labels.append(e[0])
+			vals.append(e[1])
+		if len(v)<n:
+			for _ in range(n-len(v)):
+				labels.append('')
+				vals.append(0)
+
+	N = len(top_words.keys())
+
+	x = list(range(N)) * n
+	y = []
+	for i in range(n,0,-1):
+		y+=N*[i]
+
+	circle_sizes = []
+	for i in range(n):
+		circle_sizes+=vals[i::n]
+
+	color_list = []
+	for i in range(n-1,-1,-1):
+		color_list+=[colors[i]]*N
+
+	label_list = []
+	for i in range(n):
+		label_list+=labels[i::n]
+
+	fig = go.Figure(data=[go.Scatter(
+		x=x,
+		y=y,
+		mode='markers',
+		marker=dict(
+			color=color_list,
+			size=circle_sizes,
+			sizemode='area',
+			sizeref=max(vals)/r
+		)
+	)])
+
+
+	top_n_labels = ['Top 3rd word', 'Top 2nd word', 'Top 1st word']
+
+	for k in range(4,n+1):
+		top_n_labels.insert(0,'Top {}th word'.format(k))
+
+	xaxis = list(top_words.keys())
+	xaxis_ticks = [get_human_readable_quarter_name(e) for e in xaxis]
+
+	fig.update_layout(
+		title='Top-{} most frequent words/phrases searched per quarter'.format(n),
+		xaxis = dict(
+			tickmode = 'array',
+			tickangle = -45,
+			tickvals = list(range(N)),
+			ticktext = xaxis_ticks,
+		),    
+		yaxis = dict(
+			tickmode = 'array',
+			tickvals = list(range(1,n+1)),
+			ticktext = top_n_labels[-n:]
+		)
+	)
+
+	for i in range(n*N):
+		fig.add_annotation(x=x[i], y=y[i],text=label_list[i],showarrow=False,textangle=-45)
+
+	fig.show()
 
 
 
